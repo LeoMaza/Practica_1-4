e2514f2fa09eaab402169df99b5faed9e5f92ed3
Leo Maza <leomaza2222@gmail.com>
2022-06-02 11:01:33 -0300

    practica 1-4

---
+++ algoritmo.js
@@ -0,0 +1,17 @@
+document.write( `
+<details>
+    <summary>Info 1</summary>
+    <p title="texto">
+        Texto de detalles
+    </p>
+    <img title="arboles" width="300" height="200" src="https://www.ecured.cu/images/thumb/5/5a/%C3%81rboll.jpg/800px-%C3%81rboll.jpg" alt="imagen de arboles">
+    <br>
+    <a href="https://netflix.com" title="Ingresa a la pagina de netflix">Netflix</a>
+</details>
+<details>
+    <summary>Info 2</summary>
+    <p>
+        Texto de detalles
+    </p>
+</details>
+` );
